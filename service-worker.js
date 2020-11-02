cdac007af6e5956565a5bbbab27eabe93ca72289
Sumanth Ratna <sumanthratna@gmail.com>
2020-11-02 11:08:21 -0500

    Update 2020-11-02T16:08:16Z

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7902fceda0a18e457dc3649c177a7cb7.js"
+  "/precache-manifest.20b43621a9f21e1cb4152bd46f8d4cac.js"
 );
 
 self.addEventListener('message', (event) => {
